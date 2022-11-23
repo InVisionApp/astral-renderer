4c8e9d9f4a39f5816af38c6f8770905c30e41a7b
Kevin Rogovin <kevinrogovin@invisionapp.com>
2022-11-23 12:42:23 +0200

    astral/util/matrix: make robust against catastrophic cancellation on smaller singular computation

--- src/astral/util/matrix.cpp
+++ src/astral/util/matrix.cpp
@@ -23,8 +23,7 @@ astral::vec2
 compute_singular_values_squared(const astral::float2x2 &M)
 {
   float a, b, c, d, ad_bc;
-  float B, C, desc;
-  astral::vec2 return_value;
+  float B, C;
 
   /* Given a matrix M, the singular value decomposition of
    * M is given by:
@@ -47,6 +46,41 @@ compute_singular_values_squared(const astral::float2x2 &M)
    * singular values of M. Since N is 2x2 we can use the
    * quadratic formula to get the eigen values of N
    * easily.
+   *
+   *     | a^2 + b^2        ac + bd  |
+   * N = |                           |
+   *     |  ac + bd        c^2 + d^2 |
+   *
+   *
+   * the eigen values are the solutions to the quadratic polynomial
+   *
+   * f(t) = det(tI - N)
+   *
+   * which (after algebra) is given by
+   *
+   * f(t) = t^2 - Bt + C
+   *
+   * where
+   *
+   *  B = a^2 + b^2 + c^2 + d^2
+   *  C = (ad - bc)^2
+   *
+   * and thus the return value is
+   *
+   * t0 = 0.5 * (B + sqrt(D))
+   * t1 = 0.5 * (B - sqrt(D))
+   *
+   * where D = B^2 - 4C
+   *
+   * In addition, because the matrix N is positive semidefinite
+   * via for any vector v,
+   *
+   *  <v, Nv> = <v, M(transpose(M)(v)) >
+   *          = <transpose(M)(v), transpose(M)(v)>
+   *          >= 0
+   *
+   * we then know that the Eigen values are real, though
+   * possibly zero. Hence, we know that D >= 0 as well.
    */
   a = M.row_col(0, 0);
   b = M.row_col(0, 1);
@@ -54,20 +88,61 @@ compute_singular_values_squared(const astral::float2x2 &M)
   d = M.row_col(1, 1);
 
   B = a * a + b * b + c * c + d * d;
+  if (B <= 0.0f)
+    {
+      /* we have, up to numerical accuracy that M is the
+       * zero matrix, return 0's.
+       */
+      return astral::vec2(0.0f, 0.0f);
+    }
+
   ad_bc = (a * d - b * c);
   C = ad_bc * ad_bc;
 
-  /* in exact arithmatic B * B >= 4 * C, but we max with 0
-   * to prevent numerical rounding errors from taking a
-   * negative sqrt.
-   */
-  desc = astral::t_sqrt(astral::t_max(0.0f, B * B - 4.0f * C));
+  if (C > 0.0f)
+    {
+      float desc;
 
-  /* in exact arithmatic B >= desc, but we max with 0
-   * to prevent numerical rounding errors from giving
-   * a negative return value.
-   */
-  return 0.5f * astral::vec2(B + desc, astral::t_max(0.0f, B - desc));
+      /* in exact arithmatic B * B >= 4 * C, but we max with 0
+       * to prevent numerical rounding errors from taking a
+       * negative sqrt.
+       */
+      desc = astral::t_sqrt(astral::t_max(0.0f, B * B - 4.0f * C));
+
+      /* We know that B >= 0, thus B + desc does not
+       * have catostrophic cancellation. However,
+       * B - desc does have such cancellation.
+       *
+       *              (B - desc) (B + desc)
+       *  B - desc = ----------------------
+       *                  B + desc
+       *
+       *
+       *              B * B - (B * B - 4 * C)
+       *           = ------------------------
+       *                    B + desc
+       *
+       *                4 * C
+       *           = ----------
+       *              B + desc
+       *
+       * We have already ruled out for B to be zero
+       * from the earlier early return, so it is safe
+       * to divide by B + desc
+       */
+      float B_plus_desc;
+      astral::vec2 return_value;
+
+      B_plus_desc = B + desc;
+      return_value[0] = 0.5f * B_plus_desc;
+      return_value[1] = 2.0f * C / B_plus_desc;
+
+      return return_value;
+    }
+  else
+    {
+      return astral::vec2(B, B);
+    }
 }
 
 astral::vecN<float, 2>
