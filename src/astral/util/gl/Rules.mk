5533901b73da371c80821576729440ad49b86149
Kevin Rogovin <kevinrogovin@invisionapp.com>
2023-01-16 22:52:40 +0200

    astral/util/gl: remove gl_uniform
    It was not actually used and had some quite awful macro and template heavy code.

--- src/astral/util/gl/Rules.mk
+++ src/astral/util/gl/Rules.mk
@@ -5,7 +5,7 @@ d		:= $(dir)
 # End standard header
 
 ASTRAL_SOURCES += $(call filelist, gl_shader_source.cpp \
-	gl_uniform_implement.cpp gl_context_properties.cpp \
+	gl_context_properties.cpp \
 	gl_binding.cpp gl_get.cpp gl_program.cpp \
 	unpack_source_generator.cpp \
 	gl_shader_symbol_list.cpp)
